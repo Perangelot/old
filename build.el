22f68123dfe4d46bd1052cbe22a917c7576b8b6e
Perangelot <vitusschaefftlein@live.de>
2022-01-14 03:10:12 +0100

    Update build.el

--- build.el
+++ build.el
@@ -1,9 +1,22 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
 ;; Load the publishing system
 (require 'ox-publish)
 
-(message "Build complete!")
-
-;; Customize the HTML output; org-html-head bestimmt Aussehen!
+;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
@@ -11,8 +24,8 @@
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list ; 2 verschachtelte Listen, weil jede Liste einem Projekt entspricht
-       (list "blog"
+      (list
+       (list "org-site:main"
              :recursive t
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
@@ -23,7 +36,7 @@
              :section-numbers nil       ;; Don't include section numbers
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
-
 ;; Generate the site output
-;; The org-publish-all function at the end tells Org to publish every project configured in org-publish-project-alist! The t parameter tells it to regenerate all files regardless of when they were last generated
-(org-publish-all t) 
+(org-publish-all t)
+
+(message "Build complete!")
