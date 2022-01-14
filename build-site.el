a175be3d6abfff18fde9330f8d6d149cd010cd3b
Perangelot <vitusschaefftlein@live.de>
2022-01-14 18:31:31 +0100

    co

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"/layout/dark.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"/public/layout/dark.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
